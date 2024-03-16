4b37aa2cbdcf587e06aaafbb92da9697aac05625
Khanh Chung <khanh.d.chung@gmail.com>
2024-03-15 23:06:36 -0700

    Finished RT2 Assignment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,54 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     let emojis = ["👻", "🕷️", "🕸️", "😈", "💀", "🎃", "🧙", "👹", "👺"]
-    @State var cardCount = 4
     
     var body: some View {
         VStack {
             ScrollView {
                 cards
             }
-            Spacer()
-            cardCountAdjusters
         }
         .padding()
     }
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
+            ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-    }
-    
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-    }
-    
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button {
-            cardCount += offset
-        } label: {
-            Image(systemName: symbol)
-                .imageScale(.large)
-                .font(.largeTitle)
-        }
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
-    }
-    
-    var cardAdder: some View {
-        cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
     }
 }
 
